37f90673ab92865ba5de03018b78b8436f4ac9cc
Moises Martinez Liranzo <moisesmartinezliranzo@gmail.com>
2020-04-09 20:24:00 -0400

    simple node app with test

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+
+    res.send('hello world');
+
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
